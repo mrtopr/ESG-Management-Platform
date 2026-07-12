cf832c5da44181427c47b495a5ca9b37cce9fcf4
mrtopr <krsachin7488@gmail.com>
2026-07-12 12:01:28 +0530

    chore(cleanup): remove deprecated cn.ts file

--- FRONTEND/src/utils/cn.ts
+++
@@ -1,8 +0,0 @@
-import { type ClassValue, clsx } from "clsx"
-import { extendTailwindMerge } from "tailwind-merge"
-
-const customTwMerge = extendTailwindMerge({})
-
-export function cn(...inputs: ClassValue[]) {
-  return customTwMerge(clsx(inputs))
-}
